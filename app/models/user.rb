cb3e68bf20778f028cb72f7bf3a05368f0ac17df
Jesus Rivas <jesuswrivas@gmail.com>
2023-07-04 12:22:11 -0500

    Post and Comment models created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
 has_secure_password
 
-validate :username, presence: true , length: { minimum: 5 }
-validates :email, presence: true, format: { with: /\A\w+@\w+\z/ }
+validates :username, presence: true , length: { minimum: 5 }
+validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 validates :role, presence: true, inclusion: { in: [1, 2] }
 
 has_many :posts
-has_many :comments, through: :posts
+has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_162720) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_171532) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -20,4 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_162720) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
